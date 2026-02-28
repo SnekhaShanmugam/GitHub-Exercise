cee7b67bb60206ba9bf53c49805be084e553ad4d
SnekhaShanmugam <shs7cob@bosch.com>
2026-02-28 09:34:56 +0000

    After Refractoring

--- HelloWorldApp/obj/Debug/net10.0/HelloWorldApp.AssemblyInfo.cs
+++ HelloWorldApp/obj/Debug/net10.0/HelloWorldApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloWorldApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5a2e9830fccb073a53afa623d14b92afbdd3bd4e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2cd95094aef0e953bde4cfa10217fb9c23b7e3b6")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloWorldApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloWorldApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
